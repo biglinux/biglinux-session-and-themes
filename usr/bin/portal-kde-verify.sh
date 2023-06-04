1da8810771098a2af5624add705f326a49c5a0ad
Bruno Gonçalves <bigbruno@gmail.com>
2023-06-04 15:25:14 -0300

    Trying fix portal kde

--- usr/bin/portal-kde-verify.sh
+++ usr/bin/portal-kde-verify.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# try 30 times, one per second to make porta desktop kde work fine
+# try 30 times, one per second to make portal desktop kde work fine
 MAX_ATTEMPTS=30
 SLEEP_TIME=1
 
@@ -29,9 +29,31 @@ while [ $attempt -le $MAX_ATTEMPTS ]; do
     attempt=$((attempt+1))
 done
 
-
 # Fix GTK theme in live mode or first login after install
 if [ -e "/livefs-pkgs.txt" ] || [ -n "$BIG_THEME" ]; then
-    qdbus org.kde.GtkConfig /GtkConfig org.kde.GtkConfig.setGtkTheme $(grep 'gtk-theme-name=' "$HOME/.config/gtk-3.0/settings.ini" | cut -f2-5 -d=)
+
+    # Now to fix GTK3 theme in live mode and first boot, try 30 times, one per second to make gtk work fine
+    attempt=1
+    while [ $attempt -le $MAX_ATTEMPTS ]; do
+        result=$(qdbus org.kde.GtkConfig /GtkConfig org.kde.GtkConfig.setGtkTheme $(grep 'gtk-theme-name=' "$HOME/.config/gtk-3.0/settings.ini" | cut -f2-5 -d=))
+
+        if [ $? -eq 0 ]; then
+            # GTK OK
+            exit 0
+        else
+            # Portal fail retry
+
+            if [ $attempt -eq $MAX_ATTEMPTS ]; then
+                # fail 30 times, exit without working
+                exit 1
+            fi
+
+            # Wait a second to retry
+            sleep $SLEEP_TIME
+        fi
+
+        attempt=$((attempt+1))
+    done
+    
 fi
 
