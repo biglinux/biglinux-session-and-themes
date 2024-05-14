cf6760aa3af30c7d965940a82a8c5f8533ff8cde
gitbarnabedikartola <110747604+gitbarnabedikartola@users.noreply.github.com>
2024-05-14 20:59:44 -0300

    cp files to home and remove old widgets

--- usr/share/bigbashview/apps/biglinux-themes-gui/after-plasma6-update-dialog.sh
+++ usr/share/bigbashview/apps/biglinux-themes-gui/after-plasma6-update-dialog.sh
@@ -6,5 +6,18 @@ export TEXTDOMAIN=biglinux-session-and-themes
 
 Title=$"Temas e Desktop"
 
+# Copy igLinux Dark.colorscheme
+if [[ ! -e "$HOME/.local/share/konsole/BigLinux Dark.colorscheme" ]]; then
+    cp "/etc/skel/.local/share/konsole/BigLinux Dark.colorscheme" "$HOME/.local/share/konsole/BigLinux Dark.colorscheme"
+fi
+
+# Copy Bash.profile
+if [[ ! -e "$HOME/.local/share/konsole/Bash.profile" ]]; then
+    cp "/etc/skel/.local/share/konsole/Bash.profile" "$HOME/.local/share/konsole/Bash.profile"
+fi
+
+# BKP plasmoids
+mv "$HOME/.local/share/plasma/plasmoids/" "$HOME/.local/share/plasma/plasmoids-old/"
+
 cd /usr/share/bigbashview/apps/biglinux-themes-gui/
 bigbashview -s 900x700 -n 'biglinuxthemesgui' -p 'biglinuxthemesgui' -c transparent -w frameless after-plasma6-update-dialog.html
